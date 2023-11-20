6632df911cb20d110d1413f4dddf85c6c8002335
Jeroen Geerdink <jeroenek@gmail.com>
2023-11-20 14:26:25 +0100

    Update lego_controller.py
    clean up

--- lego_controller.py
+++ lego_controller.py
@@ -2,8 +2,6 @@ import asyncio
 from bleak import BleakScanner, BleakClient
 import time
 
-device_address = "4BE0413D-EEC3-5108-695E-00408FD59605"
-
 UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 UART_RX_CHAR_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 UART_TX_CHAR_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -22,7 +20,6 @@ class LegoController:
         self.onReady = onReady
         self.processing = False
         self.response = ""
-        #self.device = self.backend.connect(device_address)
 
     async def connect(self):
         device = await BleakScanner.find_device_by_name(self.name)
@@ -80,17 +77,7 @@ async def main():
         print("Ready")
     controller = LegoController("Pega One", callBack)
     await controller.connect()
-    time.sleep(5)
-    await controller.execute("drive","5")
     print(controller.event)
-    #controller.wait()
-    await controller.execute("drive","5")
-    print(controller.event)
-    #controller.wait()
-    await controller.execute("turn","90")
-    print(controller.event)
-    #controller.wait()
-    time.sleep(5)
 
 
 if __name__ == '__main__':
